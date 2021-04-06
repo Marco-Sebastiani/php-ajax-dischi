db33a61f637a0e7ef15ac8cb6cab71a4a8847052
Marco.seb <marco.seba96@gmail.com>
2021-04-06 18:24:28 +0200

    Change file

--- milestone-1/index.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    include 'db.php';
-    include __DIR__ . '/src/partials-php/head.php';
-    include __DIR__ . '/src/partials-php/header.php';
-    include __DIR__ . '/src/partials-php/main.php';
-    include __DIR__ . '/src/partials-php/footer.php';
-?>
--- milestone-1/webpack.mix.js
+++
@@ -1,9 +0,0 @@
-let mix = require('laravel-mix');
-
-mix
-    .js('src/script.js', 'js')
-    .sass('src/style.scss', 'css')
-    .options({
-        processCssUrls: false
-    });
-
